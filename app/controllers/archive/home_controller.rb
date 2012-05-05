c7bc6e04f8fb17e9559414108300bd7369a83c62
Jørgen Rieck <jurgemaister@gmail.com>
2012-05-05 14:46:42 +0200

    Reading and writing files to ark now works

--- app/controllers/archive/home_controller.rb
+++ app/controllers/archive/home_controller.rb
@@ -2,7 +2,12 @@ module Archive
   class HomeController < ApplicationController
     def index
       shampoo = SOAP::new
-      @data = shampoo.get_fonds_series
+      # @data = shampoo.get_specific_series("test")
+      # @data = shampoo.create_file("test", "railsTest2", "Test from RoR2", "rortest", "automagic", "rails")
+      # @data = shampoo.create_child_file("d960e224-0d23-4dbf-9478-ba336247010e", "railsChild", "Sounds naughtier that it is", "rortest", "electronic", "rails")
+      #@data = shampoo.create_record("97419d1d-046b-44b0-8f58-0bb4aa803cfe", "railsRecord", "Rails world record", "rortest", "electronic", "rails")
+      #@data = shampoo.create_file("09f0ecfc-06c3-47a4-85f7-5636eb3e4aad", "Image/PNG", "rails upload 2", "rortest", "rails", "/Volumes/Ekkokammer/jr/Pictures/Dogcow.png")
+      @data = shampoo.get_file("1a0c133b-96e0-4a6b-9a02-7c6842882f9a")
     end
   
   end
---
+++ app/models/archive/soap.rb
@@ -0,0 +1,154 @@
+require "savon"
+require "base64"
+
+module Archive
+  class SOAP
+    def initialize
+      @@client = Savon::Client.new("http://ark1.hio.no/n5/Noark5BaseService3?wsdl")
+      @@fondUUID = "66b2ed65-901c-437d-9d72-564dd00b3ef1" # Scriptorium
+     # @@seriesUUID = "a290da90-a312-47ca-8c65-8505cd46bb64" # test
+    end
+    
+    def get_fonds_series
+      result = @@client.request :ser, :fonds_get_series, body: @@fondUUID
+      data = result.to_hash[:fonds_get_series_response][:item]
+    end
+    
+    def get_specific_series(title)
+      data = get_fonds_series
+      data.each do |s|
+        if s[:title] == title
+          return s
+        end
+      end
+    end
+    
+    def get_specific_series_uuid(title)
+      series = get_specific_series(title)
+      series[:system_id]
+    end
+    
+    def create_file(series, title, officialTitle, desc, docMedium, createdBy)
+      parent_id = get_specific_series_uuid(series)
+      result =  @@client.request :ser, :case_create_file do |soap|
+                  soap.xml = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ser=\"http://service3.base.module.n5ic/\">
+                     <soapenv:Header/>
+                     <soapenv:Body>
+                        <ser:seriesSystemId>"+parent_id+"</ser:seriesSystemId>
+                        <ser:parentFileSystemId></ser:parentFileSystemId>
+                        <ser:title>"+title+"</ser:title>
+                        <ser:officialTitle>"+officialTitle+"</ser:officialTitle>
+                        <ser:description>"+desc+"</ser:description>
+                        <ser:documentMedium>"+docMedium+"</ser:documentMedium>
+                        <ser:createdBy>"+createdBy+"</ser:createdBy>
+                     </soapenv:Body>
+                  </soapenv:Envelope>"
+                end
+        result.to_hash[:case_file_create_response][:message]
+    end
+    
+    def create_child_file(parent_id, title, officialTitle, desc, docMedium, createdBy)
+      result =  @@client.request :ser, :case_create_file do |soap|
+                  soap.xml = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ser=\"http://service3.base.module.n5ic/\">
+                     <soapenv:Header/>
+                     <soapenv:Body>
+                        <ser:seriesSystemId></ser:seriesSystemId>
+                        <ser:parentFileSystemId>"+parent_id+"</ser:parentFileSystemId>
+                        <ser:title>"+title+"</ser:title>
+                        <ser:officialTitle>"+officialTitle+"</ser:officialTitle>
+                        <ser:description>"+desc+"</ser:description>
+                        <ser:documentMedium>"+docMedium+"</ser:documentMedium>
+                        <ser:createdBy>"+createdBy+"</ser:createdBy>
+                     </soapenv:Body>
+                  </soapenv:Envelope>"
+                end
+      result.to_hash
+    end
+    
+    def create_record(parent_id, title, officialTitle, desc, docMedium, createdBy)
+      result =    @@client.request :ser, :registry_entry_create do |soap|
+                    soap.xml = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ser=\"http://service3.base.module.n5ic/\">
+                       <soapenv:Header/>
+                       <soapenv:Body>
+                          <ser:caseFileSystemId>"+parent_id+"</ser:caseFileSystemId>
+                          <ser:title>"+title+"</ser:title>
+                          <ser:officialTitle>"+officialTitle+"</ser:officialTitle>
+                          <ser:description>"+desc+"</ser:description>
+                          <ser:documentMedium>"+docMedium+"</ser:documentMedium>
+                          <ser:createdBy>"+createdBy+"</ser:createdBy>
+                       </soapenv:Body>
+                    </soapenv:Envelope>"
+                  end
+        result.to_hash
+    end
+    
+    def create_file(parent_id, document_type, title, description, created_by, file)
+      result =    @@client.request :ser, :document_create do |soap|
+                    soap.xml = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ser=\"http://service3.base.module.n5ic/\">
+                       <soapenv:Header/>
+                       <soapenv:Body>
+                          <ser:reSystemId>"+parent_id+"</ser:reSystemId>
+                          <ser:documentType>"+document_type+"</ser:documentType>
+                          <ser:title>"+title+"</ser:title>
+                          <ser:description>"+description+"</ser:description>
+                          <ser:createdBy>"+created_by+"</ser:createdBy>
+                          <ser:documentMedium>electronic archive</ser:documentMedium>
+                          <ser:base64Content>"+Base64.encode64(open(file).to_a.join)+"</ser:base64Content>
+                          <ser:extension>"+File.extname(file).gsub(".","")+"</ser:extension>
+                          <ser:format>"+File.extname(file).gsub(".","")+"</ser:format>
+                       </soapenv:Body>
+                    </soapenv:Envelope>"
+                  end
+      result.to_hash
+    end
+    
+    def get_file_info(file_id)
+      result =    @@client.request :ser, :document_description_get_document_objects do |soap|
+                    soap.xml = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ser=\"http://service3.base.module.n5ic/\">
+                       <soapenv:Header/>
+                       <soapenv:Body>
+                          <ser:documentDescriptionGetDocumentObjects>"+file_id+"</ser:documentDescriptionGetDocumentObjects>
+                       </soapenv:Body>
+                    </soapenv:Envelope>"
+                  end
+      result_hash = result.to_hash[:document_description_get_document_objects_response][:item]
+                  
+      extra =     @@client.request :ser, :document_object_get_document_description do |soap|
+                    soap.xml = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ser=\"http://service3.base.module.n5ic/\">
+                       <soapenv:Header/>
+                       <soapenv:Body>
+                          <ser:documentObjectGetDocumentDescription>"+result_hash[:system_id]+"</ser:documentObjectGetDocumentDescription>
+                       </soapenv:Body>
+                    </soapenv:Envelope>"
+                  end
+      extra_hash = extra.to_hash[:document_object_get_document_description_response]
+      result_hash[:"title"] = extra_hash[:title]
+      result_hash[:"document_type"] = extra_hash[:document_type]
+      result_hash[:"description"] = extra_hash[:description]
+      return result_hash
+    end
+    
+    def get_file(file_id)
+      result = get_file_info(file_id)
+      file_result =    @@client.request :ser, :document_get do |soap|
+                    soap.xml = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ser=\"http://service3.base.module.n5ic/\">
+                       <soapenv:Header/>
+                       <soapenv:Body>
+                          <ser:documentGet>"+file_id+"</ser:documentGet>
+                       </soapenv:Body>
+                    </soapenv:Envelope>"
+                  end
+      file_hash = file_result.to_hash[:document_get_response]
+      result[:"base64_data"] = file_hash
+      decode_file(result)
+    end
+    
+    def decode_file(hash)
+      f = StringIO.new
+      open(hash[:title] +"."+ hash[:format], "w+b") do
+        f << Base64.decode64(hash[:base64_data])
+      end
+      return f
+    end  
+  end
+end
